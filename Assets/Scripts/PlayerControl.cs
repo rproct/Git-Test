8d4eec1f63270b51329cbcda17f4b8cfac26ed14
Ryan Proctor <49620154+rproct@users.noreply.github.com>
2021-03-23 14:17:40 -0700

    Added Player Control

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    private float speed = 5;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float hori = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        float vert = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        transform.Translate(hori, 0, vert);
+    }
+}
